9aebe02eb0eb6ba86b06e9bb895c070ee3788e10
qsong1012 <diana.song19931012@gmail.com>
2022-04-12 16:04:50 -0400

    removed dataset implementation from fitter

--- model/fitter.py
+++ model/fitter.py
@@ -10,6 +10,14 @@ from .helper import *
 import shutil
 from tqdm import tqdm
 
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+from dataset import IDHDataset
+from torch.utils.data import DataLoader
+
 def format_results(res):
     line = ""
     for key in sorted(res):
@@ -25,23 +33,27 @@ def format_results(res):
 def unpack_sample(sample, device):
     imgs, ids, targets = list(
         map(lambda x: sample[x], [0,1,2]))
-    imgs, ids, targets = \
-        imgs.to(device), ids.to(device), targets.to(device)
+    imgs = imgs.to(device)
+#     ids = ids.to(device) 
+    targets = targets.to(device)
     return imgs, ids, targets
 
 
 def reshape_img_batch(img_batch, crop_size):
-    return img_batch.\
-        view(img_batch.size(0), 3, -1, crop_size, crop_size).\
-        permute(0, 2, 1, 3, 4).\
-        contiguous().\
-        view(-1, 3, crop_size, crop_size)
+    return img_batch.view(-1, 3, crop_size, crop_size)
+#     return img_batch.\
+#         view(img_batch.size(0), 3, -1, crop_size, crop_size).\
+#         permute(0, 2, 1, 3, 4).\
+#         contiguous().\
+#         view(-1, 3, crop_size, crop_size)
 
 
 class HybridFitter:
     def __init__(
             self,
             model,
+            transform,
+            dataloader,
             checkpoint_to_resume='',
             writer=None,
             args=None,
@@ -51,11 +63,11 @@ class HybridFitter:
         self.writer = writer
         self.args = args
         self.criterion = loss_function
-
+        self.transform = transform
         self.model = model
         self.device = self.model.device
         self.reset_optimizer()
-        self.dataloaders = {}
+        self.dataloaders = dataloader
         self.meta_df = {}
         self.es = EarlyStopping(patience=self.args.patience, mode='max')
         self.timestr = timestr
@@ -85,71 +97,7 @@ class HybridFitter:
         self.schedulers['sgd'].load_state_dict(ckp['state_dict_scheduler_sgd'])
         self.current_epoch = ckp['epoch'] + 1
 
-    def get_datasets(
-            self,
-            pickle_file,
-            mode='train',
-    ):
-        if mode == 'train':
-            batch_size = self.args.num_patches * self.args.batch_size
-            num_crops = self.args.num_crops
-        elif mode == 'val':
-            batch_size = self.args.num_val
-            num_crops = self.args.num_crops
-        else:
-            batch_size = self.args.batch_size
-            num_crops = self.args.num_crops
 
-        transform = get_data_transforms()[mode]
-
-        ds = SlidesDataset(
-            data_file=pickle_file,
-            image_dir='./',
-            crop_size=self.args.crop_size,
-            num_crops=num_crops,
-            outcome=self.args.outcome,
-            outcome_type=self.args.outcome_type,
-            transform=transform
-        )
-        self.dataloaders[mode] = torch.utils.data.DataLoader(
-            ds,
-            shuffle=False,
-            batch_size=batch_size,
-            num_workers=self.args.num_workers,
-            pin_memory=False,
-            drop_last=True if mode == 'train' else False
-        )
-
-    def prepare_datasets(
-            self,
-            pickle_file,
-            mode='train',
-            batch_size=256):
-        self.batch_size = batch_size
-        if isinstance(pickle_file, str):
-            _df = pd.read_pickle(pickle_file)
-        else:
-            _df = pickle_file
-
-        if mode == 'train':
-            if self.args.sampling_ratio is None:
-                sampling_ratio = None
-            else:
-                sampling_ratio = list(map(int, self.args.sampling_ratio.split(',')))
-            self.meta_df[mode] = grouped_sample(
-                    _df, 
-                    stratify_var=self.args.stratify, 
-                    weights=sampling_ratio, 
-                    num_obs=len(_df.submitter_id.unique())*self.args.repeats_per_epoch,
-                    num_patches=self.args.num_patches,
-                    patient_var='submitter_id')
-
-            self.writer['meta'].info(self.meta_df[mode].shape)
-        elif self.args.sample_id:
-            self.meta_df[mode] = _df.groupby('submitter_id', group_keys=False).apply(
-                lambda x: x.sample(self.args.num_val, replace=True))
-        else:
-            self.meta_df[mode] = _df
 
     def reset_optimizer(self):
         conv_params = []
@@ -189,13 +137,7 @@ class HybridFitter:
             T_mult=self.args.cosine_t_mult)
 
     def train(self, df_train=None, epoch=0):
-        self.prepare_datasets(
-            df_train,
-            'train',
-            batch_size=self.args.batch_size * self.args.num_patches)
-        self.get_datasets(
-            self.meta_df['train'],
-            'train')
+
 
         self.writer['meta'].info('Training from step %s' % epoch)
         # training phase
@@ -238,17 +180,20 @@ class HybridFitter:
             # forward and backprop
             self.optimizers['sgd'].zero_grad()
             with torch.set_grad_enabled(True):
-                ppi = self.args.num_patches * self.args.num_crops
+#                 ppi = self.args.num_patches * self.args.num_crops
+                ppi = self.args.num_patches
+#                 print('ppi:',ppi)
                 model_outputs = self.model(train_imgs, ppi)
+#                 print('inputs:', train_imgs)
                 train_preds = model_outputs['pred']
 
-                train_targets = train_targets.view(nbatches, self.args.num_patches, -1)[:, 0, :]
-                train_loss = self.criterion.calculate(train_preds, train_targets)
+#                 train_targets = train_targets.view(nbatches, self.args.num_patches, -1)[:, 0, :]
+                train_loss = self.criterion(train_preds, train_targets)
                 train_loss.backward()
 
                 eval_t.update(
                     {
-                        "ids": train_ids,
+#                         "ids": train_ids,
                         "preds": train_preds,
                         "targets": train_targets
                     }
@@ -268,7 +213,7 @@ class HybridFitter:
 
             # update metrics
             losses.update(train_loss.item(), nbatches)
-
+         
             train_metrics = calculate_metrics(
                 train_preds.data.cpu().numpy(),
                 train_targets.data.cpu().numpy(),
@@ -281,20 +226,20 @@ class HybridFitter:
 
             if (i + 1) % self.args.log_freq == 0:
                 progress.display(i + 1)
-
+        
         train_res = eval_t.evaluate()
         train_res['epoch'] = epoch
         train_res['mode'] = 'train'
         return train_res
 
-    def evaluate(self, df_val, epoch=0):
-        self.prepare_datasets(
-            df_val,
-            'val',
-            batch_size=self.args.batch_size)
-        self.get_datasets(
-            self.meta_df['val'],
-            'val')
+    def evaluate(self, df_val = None, epoch=0):
+#         self.prepare_datasets(
+#             df_val,
+#             'val',
+#             batch_size=self.args.batch_size)
+#         self.get_datasets(
+#             self.meta_df['val'],
+#             'val')
 
         self.writer['meta'].info('Starting evaluation')
         # validation phase
@@ -316,11 +261,11 @@ class HybridFitter:
             # forward
             with torch.set_grad_enabled(False):
                 val_preds = self.model(val_imgs, self.args.num_val*self.args.num_crops)['pred']
-                val_targets = val_targets.view(nbatches, self.args.num_val, -1)[:, 0, :]
-                val_ids = val_ids.view(nbatches, self.args.num_val, -1)[:, 0, :]
+#                 val_targets = val_targets.view(nbatches, self.args.num_val, -1)[:, 0, :]
+#                 val_ids = val_ids.view(nbatches, self.args.num_val, -1)[:, 0, :]
                 eval_v.update(
                     {
-                        "ids": val_ids,
+#                         "ids": val_ids,
                         "preds": val_preds,
                         "targets": val_targets
                     }
@@ -331,24 +276,29 @@ class HybridFitter:
         val_res['mode'] = 'val'
         save_path = os.path.join("predictions",self.model_name,"%04d.csv" % epoch)
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
+#         print(eval_v.values)
         eval_v.save(save_path)
         return val_res
 
-    def fit_epoch(self, data_dict, epoch=0):
-
+    def fit_epoch(self, data_dict = None, epoch=0):
         train_res = self.train(
-            data_dict['train'],
+#             data_dict['train'],
             epoch=epoch)
+        self.writer['data'].info(format_results(train_res))
+        self.schedulers['adam'].step()
+        self.schedulers['sgd'].step()
+        
+        
         val_res = self.evaluate(
-            data_dict['val'],
+#             data_dict['val'],
             epoch=epoch)
 
-        self.writer['data'].info(format_results(train_res))
+
         self.writer['data'].info(format_results(val_res))
 
-        ########################################
-        # schedule step
-        ########################################
+    ########################################
+    # schedule step
+    ########################################
 
         if self.args.outcome_type == 'survival':
             performance_measure = torch.tensor(val_res['c-index'])
@@ -357,8 +307,7 @@ class HybridFitter:
         elif self.args.outcome_type == 'regression':
             performance_measure = torch.tensor(val_res['r2'])
 
-        self.schedulers['adam'].step()
-        self.schedulers['sgd'].step()
+
 
         is_best = False
         if performance_measure > self.best_metric:
@@ -373,7 +322,7 @@ class HybridFitter:
             checkpoints_folder=self.checkpoints_folder
             )
 
-        if epoch >= 100:
+        if epoch >= self.args.epochs:
             if self.es.step(performance_measure):
                 return 1  # early stop criterion is met, we can stop now
         return 0
@@ -411,7 +360,7 @@ class HybridFitter:
 
     def fit(
         self,
-        data_dict,
+#         data_dict,
         checkpoints_folder='checkpoints'
     ):
 
@@ -419,7 +368,7 @@ class HybridFitter:
 
         for epoch in range(self.current_epoch, self.args.epochs + 1):
             return_code = self.fit_epoch(
-                data_dict,
+#                 data_dict,
                 epoch=epoch)
             if return_code:
                 break
