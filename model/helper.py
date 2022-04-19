5f59f674af4a271d009bfdeb63d83b4d98a68f44
qsong1012 <diana.song19931012@gmail.com>
2022-04-19 17:18:54 -0400

    remove loss and data related functions

--- model/helper.py
+++ model/helper.py
@@ -62,36 +62,6 @@ def get_filename_extensions(args):
     return ext_data, ext_experiment, ext_split
 
 
-###########################################
-#             Loss function               #
-###########################################
-
-
-def log_parlik_loss_cox(scores, times=None, events=None):
-    '''
-    scores: values predicted by CNN model
-    times: follow-up time
-    events: 1 for event 0 for censor
-    '''
-    scores = scores.view(-1)
-    times = times.view(-1)
-    events = events.float().view(-1)
-
-    scores = torch.clamp(scores, max=20)  # to avoid too large value after exponential
-    scores_exp = torch.exp(scores)
-    idx = torch.argsort(times, descending=True)
-    times = times[idx]
-    scores = scores[idx]
-    scores_exp = scores_exp[idx]
-    events = events[idx]
-    log_scores = torch.log(torch.cumsum(scores_exp, dim=0) + 1e-5)
-    uncensored_likelihood = scores - log_scores
-    censored_likelihood = torch.mul(uncensored_likelihood, events.type(torch.float32)).sum()
-    num_events = events.sum()
-    loss = torch.div(-censored_likelihood, num_events)
-    return loss
-
-
 ########################################
 #               Early stopping         #
 ########################################
@@ -192,48 +162,6 @@ class MobileNetV2Updated(nn.Module):
         x = self.fc(x)
         return x
 
-########################################
-#          balanced sampling           #
-########################################
-# On the patient level, each epoch might be too small (several hundred patients).
-# We can repeat all the samples multiple times and randomize them to obtain a larger
-# epoch size.
-# Specifically, for survival analysis, we can fixed the ratio of e (events) and ne 
-# (no events) to ensure each batch contains some events.
-
-def unique_shuffle_to_list(x):
-    x = x.unique()
-    np.random.shuffle(x)
-    return x.tolist()
-
-class FlexLoss:
-    def __init__(self, outcome_type, class_weights=None, device=torch.device('cpu')):
-        assert outcome_type in ['survival', 'classification', 'regression']
-        if class_weights is None:
-            pass
-        else:
-            class_weights = torch.tensor(class_weights).to(device)
-
-        if outcome_type == 'survival':
-            self.criterion = log_parlik_loss_cox
-        elif outcome_type == 'classification':
-            self.criterion = nn.CrossEntropyLoss(weight=class_weights)
-        else:
-            self.criterion = nn.MSELoss()
-        self.outcome_type = outcome_type
-
-    def calculate(self, pred, target):
-        if self.outcome_type == 'survival':
-            time = target[:, 0].float()
-            event = target[:, 1].int()
-            return self.criterion(pred, time, event)
-
-        elif self.outcome_type == 'classification':
-            return self.criterion(pred, target.long().view(-1))
-
-        else:
-            return self.criterion(pred, target.float())
-
 
 def grouped_sample(data, stratify_var, weights, num_obs=10000, num_patches=4, patient_var='submitter_id', patch_var='file'):
     #################################
@@ -290,43 +218,6 @@ def reverse_norm(mean, std):
     std = torch.tensor(std, dtype=torch.float32)
     return (-mean / std).tolist(), (1 / std).tolist()
 
-def get_data_transforms(patch_size=224):
-
-    data_transforms = {
-        'train':
-        transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.RandomCrop(patch_size),
-            transforms.ColorJitter(brightness=0.35,
-                                   contrast=0.5,
-                                   saturation=0.1,
-                                   hue=0.16),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                PATH_MEAN, PATH_STD
-            )  # mean and standard deviations for lung adenocarcinoma resection slides
-        ]),
-        'val':
-        transforms.Compose([
-            transforms.RandomCrop(patch_size),
-            transforms.Normalize(PATH_MEAN, PATH_STD)
-        ]),
-        'predict':
-        transforms.Compose([
-            transforms.Normalize(PATH_MEAN, PATH_STD)
-        ]),
-        'normalize':
-        transforms.Compose([
-            transforms.Normalize(PATH_MEAN, PATH_STD)
-        ]),
-        'unnormalize':
-        transforms.Compose([
-            transforms.Normalize(*reverse_norm(PATH_MEAN, PATH_STD))
-            ]),
-    }
-    return data_transforms
 
 ###########################################
 #          CUSTOM DATALOADER              #
@@ -409,24 +300,6 @@ def batchsplit(img, nrows, ncols):
             ), ncols, axis=2)
     )
 
-
-def random_crop(img, height, width):
-    x = random.randint(0, img.shape[1] - width)
-    y = random.randint(0, img.shape[0] - height)
-    img = img[y:y + height, x:x + width]
-    return img
-
-
-def random_crops(img, crop_size, n_crops):
-    h, w, c = img.shape
-    if (h == crop_size) and (w == crop_size):
-        return img
-    nrows = h // crop_size
-    ncols = w // crop_size
-    if max(nrows % crop_size, ncols % crop_size):
-        img = random_crop(img, nrows * crop_size, ncols * crop_size)
-    splits = batchsplit(img, nrows, ncols)
-    return splits[random.sample(range(splits.shape[0]), n_crops)]
 
 
 ###########################################
